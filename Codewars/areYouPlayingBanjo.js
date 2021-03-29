337065b779e3a99eaf9cae54ba8556f76c472ce4
Petrus Soge <petrus.d.r.soge@gmail.com>
2021-03-29 23:09:17 +0700

    banjo

--- Codewars/areYouPlayingBanjo.js
+++ Codewars/areYouPlayingBanjo.js
@@ -1,8 +1,15 @@
-function areYouPlayingBanjo(name){
-    if (name[0] && name[name.length - 1] === "R") {
-        console.log(`${name} plays banjo`);
-    } else {
-        console.log(`${name} does not play banjo`);
-    }
+function areYouPlayingBanjo(name) {
+  if (name[0] && name[name.length - 1] === "R") {
+    console.log(`${name} plays banjo`);
+  } else {
+    console.log(`${name} does not play banjo`);
+  }
 }
-areYouPlayingBanjo("Rober")
+areYouPlayingBanjo("Rober");
+
+//2nd
+function areYouPlayingBanjo(name) {
+    // Implement me
+   return name + (name[0].toLowerCase() == "r" ? " plays" : " does not play") + " banjo";
+  
+     }
