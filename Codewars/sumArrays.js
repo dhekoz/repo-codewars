f95aade7ca03b3b3e62c77fc9e55afd63289b426
dhekoz <79990915+dhekoz@users.noreply.github.com>
2022-11-15 02:43:00 +0000

    second way

--- Codewars/sumArrays.js
+++ Codewars/sumArrays.js
@@ -1,4 +1,6 @@
-function sum(angka){
-    if(angka === 0) return 0;
-    return angka.reduce((total, skrg) => total + skrg);
-}
+// function sum(angka){
+//     if(angka === 0) return 0;
+//     return angka.reduce((total, skrg) => total + skrg);
+// }
+
+sum = angka => angka.reduce((awal, skrg) => awal + skrg, 0);
